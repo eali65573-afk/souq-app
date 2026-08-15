df974596cf1a2c0fd82e719f3c3778242abfd491
eali65573-afk <eali65573@gmail.com>
2026-08-15 05:14:17 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// إعدادات مشروع Firebase الخاص بالسوق المفتوح
+const firebaseConfig = {
+  apiKey: "AIzaSyATXTpf0_-VUGzlFZba0Fd8RceG68HQs4s",
+  authDomain: "souq-al-maftouh.firebaseapp.com",
+  projectId: "souq-al-maftouh",
+  storageBucket: "souq-al-maftouh.firebasestorage.app",
+  messagingSenderId: "244245007903",
+  appId: "1:244245007903:web:88d2acee851c86c5b00f2a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
